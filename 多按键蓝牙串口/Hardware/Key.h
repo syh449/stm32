cb9d83e435727cc8e09797e5ce92de3b653056d0
Shao Yuhan <2090969269@qq.com>
2026-01-28 10:56:58 +0800

    null

---
+++ 多按键蓝牙串口/Hardware/Key.h
@@ -0,0 +1,8 @@
+#ifndef __KEY_H
+#define __KEY_H
+
+void Key_Init1(void);
+void Key_Init2(void);
+uint8_t Key_GetNum(void);
+
+#endif
---
+++ 多按键蓝牙串口/User/main.c
@@ -0,0 +1,42 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "Serial.h"
+#include "Key.h"
+
+int RxData;			//定义用于接收串口数据的变量
+uint8_t KeyNum = 0;
+int main(void)
+{
+	/*模块初始化*/
+	OLED_Init();		//OLED初始化
+	Key_Init1();
+	Key_Init2();
+	/*显示静态字符串*/
+	OLED_ShowString(1, 1, "RxData:");
+	
+	/*串口初始化*/
+	Serial_Init();		//串口初始化
+	
+	while (1)
+	{
+	/*	if (Serial_GetRxFlag() == 1)			//检查串口接收数据的标志位
+		{
+			RxData = Serial_GetRxData();			//获取串口接收的数据
+			//OLED_ShowNum(1, 8,RxData, 2);	//显示串口接收的数据
+			Serial_SendByte(RxData);			//串口将收到的数据回传回去，用于测试
+			if(RxData==2)
+				OLED_ShowNum(1, 8,2, 2);
+			else
+				OLED_ShowNum(1, 8,1, 2);
+		}*/
+		
+		KeyNum=Key_GetNum();
+		
+		if(KeyNum!=0)
+		{Serial_SendByte(KeyNum);}	
+		
+		
+		
+	}
+}
